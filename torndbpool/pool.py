c3d7df5d2e082c8bf2b2a648738fccf76a86b0b8
zhengtong <zhengtong0898@aliyun.com>
2019-05-12 07:27:56 +0800

    Update pool.py
    将RLock更换为Condition(RLock)

--- torndbpool/pool.py
+++ torndbpool/pool.py
@@ -1,3 +1,4 @@
+# -.- coding:utf-8 -.-
 import threading
 from . import torndb
 from queue import Queue
@@ -10,68 +11,68 @@ class PoolException(Exception):
 
 class Pool(object):
 
-    def __init__(self, pool_size=100):
+    def __init__(self, host, database, user=None, password=None,
+                 max_idle_time=7 * 3600, connect_timeout=30,
+                 time_zone="+8:00", charset="utf8mb4",
+                 sql_mode="TRADITIONAL", pool_size=100, **kwargs):
         """
         :param pool_size: 线程池大小, 默认是100, 这个参数应该多大比较合适, 取决
         于mysql的max_connections参数的设定以及操作系统的限制, 例如linux的
         open files, open process等因素, 更多讨论 请参考:
         https://stackoverflow.com/questions/39976756/the-max-connections-in-mysql-5-7
+        :param kwargs: 更多参数请参考这里 https://github.com/PyMySQL/PyMySQL/blob/master/pymysql/connections.py#L116
         """
-        self.lock = threading.RLock()
-        self.queue = Queue()
-        self.actives = []
+        self.cond = threading.Condition(threading.RLock())
+        self._idles = Queue()
+        self._busies = []
         self.pool_size = pool_size
+        self.db_kwargs = dict(host=host, database=database, user=user,
+                               password=password, max_idle_time=max_idle_time,
+                               connect_timeout=connect_timeout,
+                               time_zone=time_zone, charset=charset,
+                               sql_mode=sql_mode, pool=self, **kwargs)
 
-    def get(self, connect_factory=None, timeout=30):
-        with self.lock:
+    def connect(self):
+        connect_factory = partial(Connection, **self.db_kwargs)
+        return self._connect(connect_factory)
 
-            if not self.idles():
-                if self.pool_size > len(self.actives):
-                    conn = connect_factory()
-                else:
-                    conn = self.queue.get(timeout=timeout)
+    def _connect(self, connect_factory=None, timeout=10):
+        with self.cond:
+
+            if self.idles():
+                conn = connect_factory()
             else:
-                conn = self.queue.get(timeout=timeout)
+                if self._idles.qsize():
+                    conn = self._idles.get()
+                else:
+                    self.cond.wait(timeout)
+                    if self._idles.qsize():
+                        conn = self._idles.get()
+                    else:
+                        raise PoolException("Acquire connection time out.")
 
-            self.actives.append(conn)
+            self._busies.append(conn)
             return conn
 
     def release(self, conn):
-        with self.lock:
-
-            if conn not in self.actives:
+        with self.cond:
+            
+            if conn not in self._busies:
                 raise PoolException("Release unknown connection.")
-
-            index = self.actives.index(conn)
-            self.actives.pop(index)
-            self.queue.put(conn)
+            
+            index = self._busies.index(conn)
+            self._busies.pop(index)
+            self._idles.put(conn)
+            self.cond.notify()
 
     def idles(self):
-        with self.lock:
-            return self.queue.qsize()
-
-    def connect(self, host, database, user=None, password=None,
-                max_idle_time=7 * 3600, connect_timeout=30, time_zone="+0:00",
-                charset="utf8", sql_mode="TRADITIONAL"):
-
-        connect_factory = partial(Connection, **{
-            "host": host,
-            "database": database,
-            "user": user,
-            "password": password,
-            "max_idle_time": max_idle_time,
-            "connect_timeout": connect_timeout,
-            "time_zone": time_zone,
-            "charset": charset,
-            "sql_mode": sql_mode,
-            "pool": self
-        })
-        return self.get(connect_factory)
+        with self.cond:
+            return self.pool_size - len(self._busies)
 
     def __str__(self):
-        return "<{} pool_size={} idles={} actives={} >".format(
+        return "<{} pool_size={} idles={} busies={} >".format(
                     self.__class__.__name__,  self.pool_size,
-                    self.idles(),  len(self.actives))
+                    self.idles(),  len(self._busies))
 
 
 class Connection(torndb.Connection):
